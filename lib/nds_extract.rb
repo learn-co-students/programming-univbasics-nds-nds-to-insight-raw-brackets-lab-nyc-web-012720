afc3cb9892ace5b246538a75aaec196bc968944c
Jack T <jackmt9@gmail.com>
2020-01-17 15:32:28 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-
+require "pry"
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -21,19 +21,24 @@ def directors_totals(nds)
 result = {}
   names = []
   count1 = 0
-  count2 = 0
+  total_array = []
   while nds.length > count1
-   names << nds[count1][:name]
-   count1 += 1
-   total = 0
-   while nds[count1][movies].length > count2
-    total += nds[count1][movies][count2][:worldwide_gross]
-    count2 += 1
+    count2 = 0
+    # names << nds[count1][:name]
+    result[ nds[count1][:name] ] = 0
+    total = 0
+    while nds[count1][:movies].length > count2
+      # total += nds[count1][:movies][count2][:worldwide_gross]
+      result[nds[count1][:name]] += nds[count1][:movies][count2][:worldwide_gross]
+      count2 += 1
+    end
+    count1 += 1
+    total_array << total
+    # binding.pry
+    # result[names[count1]] = total
+    
   end
-  total_array << total
-  result[names[count1]] = total[count1]
-  end
-  
-  pp result
+      # binding.pry
+  return result
   nil
 end
