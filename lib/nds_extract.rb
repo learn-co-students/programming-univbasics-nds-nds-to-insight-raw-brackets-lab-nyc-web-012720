5253e34b8645fa37780a2c229a3c778c3c9c9301
Jared Fleming <jaredmf10@gmail.com>
2020-01-21 18:06:51 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,27 +12,27 @@ def directors_totals(nds)
   # that you know what you're starting with!
   #pp directors_database
   #
-  #
-
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {"Stephen Spielberg" => 1357566430,
-    'Russo Brothers' => 2281002470,
-    'James Cameron' => 2571020373,
-    'Spike Lee' => 256624217,
-    'Wachowski Siblings' => 806180282,
-    'Robert Zemeckis' => 1273838385,
-    'Quentin Tarantino' => 662738268,
-    'Martin Scorsese' => 636812242,
-    'Francis Ford Coppola' => 509719258
-  }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
   #
+director_index = 0
+totals = {}
+
+while director_index < nds.length do
+  director_name = nds[director_index][:name]
+  totals[director_name] = 0
+  movie_index = 0
+
+  while movie_index < nds[director_index][:movies].length do
+    totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+
+  director_index += 1
+end
   #
   # Be sure to return the result at the end!
-  result
+  totals
 end
